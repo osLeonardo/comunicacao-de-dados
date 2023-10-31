5201d2a18fadf83335bbcbe345e8720e224db8cb
Jhonny Mezzari <jhonnybif@unesc.net>
2023-10-31 19:44:58 -0300

    second-blood

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,29 +1,47 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function textToBits(text) {
-  let result = '';
+  let result = "";
   for (let i = 0; i < text.length; i++) {
     const asciiValue = text.charCodeAt(i);
-    const binary = asciiValue.toString(2).padStart(8, '0');
-    result += binary + ' ';
+    const binary = asciiValue.toString(2).padStart(8, "0");
+
+    // Contando o número de bits '1' no valor ASCII
+    const numberOfOnes = (binary.match(/1/g) || []).length;
+
+    // Calculando o bit de paridade (par ou ímpar)
+    const parityBit = numberOfOnes % 2 === 0 ? "0" : "1";
+
+    // Acrescentando o bit de paridade à representação binária do caractere
+    result += binary + parityBit + " ";
   }
   return result;
 }
-
-// Função para ler o arquivo
+//01100001
+// Função para ler o arquivo usando Promise
 function readTextFile(filePath) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Erro ao ler o arquivo:', err);
-      return;
-    }
-    const bits = textToBits(data);
-    console.log(`Conteúdo do arquivo em bits (ASCII): ${bits}`);
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject("Erro ao ler o arquivo: " + err);
+      } else {
+        resolve(data);
+      }
+    });
   });
 }
 
 // Caminho do arquivo a ser lido
-const filePath = 'exemplo.txt'; // Certifique-se de ter este arquivo no mesmo diretório
+const filePath = "exemplo.txt"; // Certifique-se de ter este arquivo no mesmo diretório
 
-// Chamando a função para ler o arquivo
-readTextFile(filePath);
+// Chamando a função para ler o arquivo usando a Promise
+readTextFile(filePath)
+  .then((data) => {
+    const bits = textToBits(data);
+    console.log(
+      `Conteúdo do arquivo em bits (ASCII + Bit de Paridade): ${bits}`
+    );
+  })
+  .catch((error) => {
+    console.error(error);
+  });
--- teste.js
+++ teste.js
@@ -1,29 +1,39 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function textToBits(text) {
-  let result = '';
+  let result = "";
   for (let i = 0; i < text.length; i++) {
     const asciiValue = text.charCodeAt(i);
-    const binary = asciiValue.toString(2).padStart(8, '0');
-    result += binary + ' ';
+    const binary = asciiValue.toString(2).padStart(8, "0");
+    const numberOfOnes = (binary.match(/1/g) || []).length;
+    const parityBit = numberOfOnes % 2 === 0 ? "0" : "1";
+    result += binary + parityBit + " ";
   }
   return result;
 }
-
-// Função para ler o arquivo
 function readTextFile(filePath) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Erro ao ler o arquivo:', err);
-      return;
-    }
-    const bits = textToBits(data);
-    console.log(`Conteúdo do arquivo em bits (ASCII): ${bits}`);
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject("Erro ao ler o arquivo: " + err);
+      } else {
+        resolve(data);
+      }
+    });
   });
 }
 
 // Caminho do arquivo a ser lido
-const filePath = 'exemplo.txt'; // Certifique-se de ter este arquivo no mesmo diretório
+const filePath = "exemplo.txt"; // Certifique-se de ter este arquivo no mesmo diretório
 
-// Chamando a função para ler o arquivo
-readTextFile(filePath);
+// Chamando a função para ler o arquivo usando a Promise
+readTextFile(filePath)
+  .then((data) => {
+    const bits = textToBits(data);
+    console.log(
+      `Conteúdo do arquivo em bits (ASCII + Bit de Paridade): ${bits}`
+    );
+  })
+  .catch((error) => {
+    console.error(error);
+  });
